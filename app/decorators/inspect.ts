6bbd09b3e84568b46df6fe1088f63835e424326d
MarceloWalther-Dev <marcelo.walther.10@gmail.com>
2021-08-13 20:07:53 -0300

    template 2 de decoraters

--- app/decorators/inspect.ts
+++ app/decorators/inspect.ts
@@ -4,7 +4,10 @@ export function inspect(){
         const metodoOriginal = descriptor.value;
         descriptor.value = (...args: Array<any>) =>{
 
+            console.log(`--- Método ${propertyKey}`)
+            console.log(`------ Parâmetros: ${JSON.stringify(args)}`)
             const retorno = metodoOriginal.apply(this, args)
+            console.log(`--------- retorno: ${JSON.stringify(retorno)}`)
             return retorno;
         }
         return descriptor;
